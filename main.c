c13f32bf42aceccd87a16dab0fdeebfba8a3e2f9
Tom Moron <tomoron@2C7.42angouleme.fr>
2024-02-03 19:09:54 +0100

    j'aime bien readline

--- main.c
+++ main.c
@@ -6,13 +6,22 @@
 /*   By: tomoron <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 21:59:20 by tomoron           #+#    #+#             */
-/*   Updated: 2024/02/02 22:12:38 by tomoron          ###   ########.fr       */
+/*   Updated: 2024/02/03 19:08:04 by tomoron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int main(void)
 {
-	printf("un super minishell\n");
+	char *res;
+
+	res = (char *)1;
+	while(res)
+	{
+		res = readline("minishell $>");
+		printf("%s\n",res);
+	}
 }
