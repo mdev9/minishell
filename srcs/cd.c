7e618de66179334c56d5c5e5837bbf8e91dcc72e
mdev9 <devriesmarijn05@gmail.com>
2024-02-17 00:00:13 +0100

    fixed cd

--- srcs/cd.c
+++ srcs/cd.c
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 21:02:54 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/02/16 22:06:55 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/02/16 23:59:37 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	cd(t_cmd *args)
 	char	*new_wd;
 
 	cwd = getcwd(0, 0);
-	if (args->next->next && args->next->next->type == ARG)
+	if (args->next && args->next->next && args->next->next->type == ARG)
 	{
 		ft_printf_fd(2, "minishell: cd: too many arguments\n");
 		return (1);
@@ -26,10 +26,8 @@ int	cd(t_cmd *args)
 	if (!args->next || args->next->type != ARG)
 		new_wd = getenv("HOME");
 	else
-	{
 		new_wd = args->next->token;
-		if (chdir(new_wd) == -1)
-			perror("minishell: cd");
-	}
+	if (chdir(new_wd) == -1)
+		perror("minishell: cd");
 	return (0);
 }
