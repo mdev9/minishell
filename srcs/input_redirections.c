4304a4aaadc9033414174e93ec2bbd256e2ce648
tomoron <tomoron@student.42angouleme.fr>
2024-04-22 17:03:26 +0200

    input redirection

--- srcs/input_redirections.c
+++ srcs/input_redirections.c
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 18:15:27 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/04/22 16:47:40 by tomoron          ###   ########.fr       */
+/*   Updated: 2024/04/22 17:02:13 by tomoron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,7 @@ int	get_in_type(t_msh *msh, t_cmd *tokens)
 		if (open_input_file(msh, &cur_token))
 			return (1);
 	}
-	if (cur_token && cur_token->next && !is_operand_type(cur_token->next) && cur_token->cmd_type != PIPE)
+	if (cur_token && cur_token->next && !is_operand_type(cur_token->next) && cur_token->cmd_type != PIPE && cur_token->next->cmd_type != PIPE)
 		return (get_in_type(msh, cur_token->next));
 	return (0);
 }
